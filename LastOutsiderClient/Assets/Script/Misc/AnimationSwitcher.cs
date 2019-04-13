6b75de7205dd95a98362f28ba7a05261fa7ce3ff
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-14 03:35:32 +0900

    Add simple title

---
+++ LastOutsiderClient/Assets/Script/Misc/AnimationSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Switch animation by enable/disable game project
+/// </summary>
+public class AnimationSwitcher : MonoBehaviour {
+    public GameObject nextAnimation;
+
+    public void Switch()
+    {
+        gameObject.SetActive(false);
+        nextAnimation.SetActive(true);
+    }
+}
