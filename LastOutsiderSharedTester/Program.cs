a84cf2e9fce3259613de343046548ba4e3cdaf65
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-14 12:58:08 +0900

    Add EncryptHelperTest

--- LastOutsiderSharedTester/Program.cs
+++ LastOutsiderSharedTester/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LastOutsiderSharedTester.Test;
 
 namespace LastOutsiderSharedTester
 {
@@ -6,7 +7,10 @@ namespace LastOutsiderSharedTester
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new EncryptHelperTest().Test();
+
+            Console.WriteLine("End of Main, Enter to End :)");
+            Console.ReadLine();
         }
     }
 }
---
+++ LastOutsiderSharedTester/Test/EncryptHelperTest.cs
@@ -0,0 +1,37 @@
+﻿using LastOutsiderShared.Socket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LastOutsiderSharedTester.Test
+{
+    public class EncryptHelperTest : BaseTest
+    {
+        protected override string Name => "Encrypt Helper Test";
+
+        protected override void TestInternal()
+        {
+            var data = new byte[1000];
+            Random random = new Random();
+            random.NextBytes(data);
+
+            var server = new EncryptHelper();
+            var client = new EncryptHelper();
+            client.GenerateRandomAESKey();
+
+            server.GenerateNewRSA();
+            client.RSAPublicKey = server.RSAPublicKey;
+
+            var encryptAESKeyRSA = client.EncryptRSA(client.AESKey);
+            var decryptAESKeyRSA = server.DecryptRSA(encryptAESKeyRSA);
+            Assert(client.AESKey, decryptAESKeyRSA);
+            
+            server.AESKey = decryptAESKeyRSA;
+
+            var encryptDataAES = server.EncryptAES(data);
+            var decryptDataAES = client.DecryptAES(encryptDataAES);
+
+            Assert(data, decryptDataAES);
+        }
+    }
+}
