13da4542f6a2a4424d0b80f5f2a32b095b159957
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-14 11:51:32 +0900

    Create empty class of GameSocket/EncryptHelper in shared

---
+++ LastOutsiderShared/Socket/EncryptHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LastOutsiderShared.Socket
+{
+    /// <summary>
+    /// 패킷 통신간 암호화를 도움
+    /// </summary>
+    public class EncryptHelper
+    {
+
+    }
+}
---
+++ LastOutsiderShared/Socket/GameSocket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LastOutsiderShared.Socket
+{
+    /*
+    GameSocket {
+        DATA HEADER - 2 BYTE (0x39, 0xbe)
+        IS PING - 1 BYTE (BOOL)
+        if(!IS PING) {
+            DATA LENGTH - 4 BYTE
+            DATA CONTENT - (DATA LENGTH) BYTE
+        }
+    }
+    */
+    /// <summary>
+    /// 클라이언트와 서버간 통신에 사용하는 소켓
+    /// </summary>
+    public class GameSocket
+    {
+
+    }
+}
