e233057797c3bfe9f20bb56cbe435c3e3fc9ec4b
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-14 11:50:47 +0900

    Create SharedTester

---
+++ LastOutsiderSharedTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LastOutsiderSharedTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
