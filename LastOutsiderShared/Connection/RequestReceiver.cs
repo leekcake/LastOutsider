8e1e6422ff94eeab654dc7c55f4c46abde749de9
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-18 19:29:16 +0900

    Add Request/Response Receiver

---
+++ LastOutsiderShared/Connection/RequestReceiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LastOutsiderShared.Connection
+{
+    public interface RequestReceiver
+    {
+        Stream OnRequest(byte[] requestData);
+
+        string Key {
+            get;
+        }
+    }
+}
---
+++ LastOutsiderShared/Connection/ResponseReceiver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LastOutsiderShared.Connection
+{
+    public interface ResponseReceiver
+    {
+        void OnResponse(byte[] response);
+    }
+}
