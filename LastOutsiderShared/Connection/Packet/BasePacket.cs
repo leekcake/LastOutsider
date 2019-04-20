dc38beb3217a4b56d55e5f491d0e1dc459824752
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-20 13:48:52 +0900

    Add BasePacket

---
+++ LastOutsiderShared/Connection/Packet/BasePacket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastOutsiderShared.Connection.Packet
+{
+    public abstract class BasePacket
+    {
+        public abstract string Key {
+            get;
+        }
+
+        public abstract byte[] Data {
+            get;
+        }
+
+        public abstract ResponseReceiver ResponseReceiver {
+            get;
+        }
+
+        protected async Task Send(GameSocket socket)
+        {
+            await socket.SendRequestAsync(Key, Data, ResponseReceiver);
+        }
+    }
+}
