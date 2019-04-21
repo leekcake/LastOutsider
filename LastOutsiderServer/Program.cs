e1a7cc3b6c5862ffedfed242a4ede258c699b4c7
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-22 01:53:15 +0900

    Cleanup GeneratedPlugin of Client

--- LastOutsiderServer/Program.cs
+++ LastOutsiderServer/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using LastOutsiderServer.Receiver;
+using LastOutsiderShared.Connection;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace LastOutsiderServer
 {
@@ -6,7 +11,19 @@ namespace LastOutsiderServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var listener = new TcpListener(IPAddress.Any, 8039);
+            listener.Start();
+            new Task(async () =>
+            {
+                while (true)
+                {
+                    var serverClient = await listener.AcceptTcpClientAsync();
+                    var socket = new GameSocket();
+                    Receivers.RegisterReceivers(socket);
+                    socket.AttachNetworkStream(serverClient.GetStream());
+                }
+            }).Start();
+
         }
     }
 }
