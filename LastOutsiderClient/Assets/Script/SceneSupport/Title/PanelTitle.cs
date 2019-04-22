90d4903c614d364e04df07c4d06de7b58d3ab6f6
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-22 17:45:53 +0900

    Add GenerateAccount in Unity

--- LastOutsiderClient/Assets/Script/SceneSupport/Title/PanelTitle.cs
+++ LastOutsiderClient/Assets/Script/SceneSupport/Title/PanelTitle.cs
@@ -4,6 +4,11 @@ using System.IO;
 using UnityEngine;
 using MessagePack;
 using LastOutsiderShared.Data;
+using System.Threading.Tasks;
+using LastOutsiderClientNetwork.Packet.Extension.Login;
+using System.Security.Cryptography;
+using LastOutsiderClientNetwork.Packet;
+using LastOutsiderShared;
 
 public class PanelTitle : MonoBehaviour
 {
@@ -24,18 +29,44 @@ public class PanelTitle : MonoBehaviour
         NetworkManager.Instance.GetGameSocketAsync(loginIfMissing: false);
     }
 
-    public async void CallbackTouch()
-    {
-        if( !File.Exists(Consts.ACCOUNT_FILE) )
-        {
+    private bool TouchProgress = false;
 
+    private void Update()
+    {
+        if(Input.GetMouseButtonUp(0) && !ConnectCanvas.Instance.gameObject.activeSelf )
+        {
+            CallbackTouch();
+        }
+    }
+
+    public async Task CallbackTouch()
+    {
+        if (TouchProgress) return; 
+        TouchProgress = true;
+
+        var account = Consts.GetAccountFromFile();
+        if(account == null)
+        {
+            var socket = await NetworkManager.Instance.GetGameSocketAsync(loginIfMissing: false);
+            var authToken = new byte[128];
+            var rnd = new RNGCryptoServiceProvider();
+            rnd.GetBytes(authToken);
+
+            account = await ConnectCanvas.Instance.CreateConnectInformation<Account>("계정 생성", (listener) =>
+            {
+                socket.GenerateAccountAsync(authToken, listener);
+            }, null).WaitAsync();
+            if(account == null)
+            {
+                return;
+            }
+            await AsyncUtil.WriteBytesAsync(Consts.ACCOUNT_FILE, MessagePackSerializer.Serialize(account));
             beforeLogin.SetActive(false);
             afterLogin.SetActive(true);
-            return;
         }
-
-        var account = MessagePackSerializer.Deserialize<Account>(File.ReadAllBytes(Consts.ACCOUNT_FILE));
-        
-        
+        else
+        {
+            //TODO: Login
+        }
     }
 }
