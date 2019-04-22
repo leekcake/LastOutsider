1c4df31c099cd3d6b20b3267b0211ff7b503ddd5
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-22 22:52:09 +0900

    Create ServerTester / ResourceRecoveryTest

---
+++ LastOutsiderServerTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using LastOutsiderServerTester.Test;
+using System;
+
+namespace LastOutsiderServerTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ResourceRecoveryTest().Test();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LastOutsiderServerTester/Test/ResourceRecoveryTest.cs
@@ -0,0 +1,42 @@
+﻿using LastOutsiderServer.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestBase;
+
+namespace LastOutsiderServerTester.Test
+{
+    public class ResourceRecoveryTest : BaseTest
+    {
+        protected override string Name => "Resource Recovery Test";
+
+        protected override void TestInternal()
+        {
+            var account = ServerDataBase.Instance.GetAccount(0);
+            if(account == null)
+            {
+                account = ServerDataBase.Instance.CreateAccount(new byte[128]);
+            }
+
+            var resource = ServerDataBase.Instance.GetResource(account.Id);
+            resource.NextRecoverTime = DateTime.UtcNow;
+            ServerDataBase.Instance.UpdateResource(resource);
+            var recovery = ServerDataBase.Instance.GetResource(account.Id);
+
+            Assert(resource.Electric + resource.ElectricRecoveryAmount, recovery.Electric);
+            Assert(resource.Food + resource.FoodRecoveryAmount, recovery.Food);
+            Assert(resource.Time + resource.TimeRecoveryAmount, recovery.Time);
+            Assert(resource.Money + resource.MoneyRecoveryAmount, recovery.Money);
+
+            resource = ServerDataBase.Instance.GetResource(account.Id);
+            resource.NextRecoverTime = DateTime.UtcNow.AddMinutes(-3);
+            ServerDataBase.Instance.UpdateResource(resource);
+            recovery = ServerDataBase.Instance.GetResource(account.Id);
+
+            Assert(resource.Electric + (resource.ElectricRecoveryAmount * 2), recovery.Electric);
+            Assert(resource.Food + (resource.FoodRecoveryAmount * 2), recovery.Food);
+            Assert(resource.Time + (resource.TimeRecoveryAmount * 2), recovery.Time);
+            Assert(resource.Money + (resource.MoneyRecoveryAmount * 2), recovery.Money);
+        }
+    }
+}
