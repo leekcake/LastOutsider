251d1ed5639fee8d6cbb4c7317c92801876dc542
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-23 21:18:09 +0900

    Add Recovery Limit Test on Recovery Test

--- LastOutsiderServerTester/Test/ResourceRecoveryTest.cs
+++ LastOutsiderServerTester/Test/ResourceRecoveryTest.cs
@@ -12,13 +12,18 @@ namespace LastOutsiderServerTester.Test
 
         protected override void TestInternal()
         {
-            var account = ServerDataBase.Instance.GetAccount(0);
+            var account = ServerDataBase.Instance.GetAccount(1);
             if(account == null)
             {
                 account = ServerDataBase.Instance.CreateAccount(new byte[128]);
             }
 
+            Console.WriteLine("Test 3 Minute");
             var resource = ServerDataBase.Instance.GetResource(account.Id);
+            resource.Money = 5000;
+            resource.Food = 5000;
+            resource.Electric = 5000;
+            resource.Time = 5000;
             resource.NextRecoverTime = DateTime.UtcNow;
             ServerDataBase.Instance.UpdateResource(resource);
             var recovery = ServerDataBase.Instance.GetResource(account.Id);
@@ -27,7 +32,9 @@ namespace LastOutsiderServerTester.Test
             Assert(resource.Food + resource.FoodRecoveryAmount, recovery.Food);
             Assert(resource.Time + resource.TimeRecoveryAmount, recovery.Time);
             Assert(resource.Money + resource.MoneyRecoveryAmount, recovery.Money);
+            Console.WriteLine("Test 3 Minute OK");
 
+            Console.WriteLine("Test 6 Minute");
             resource = ServerDataBase.Instance.GetResource(account.Id);
             resource.NextRecoverTime = DateTime.UtcNow.AddMinutes(-3);
             ServerDataBase.Instance.UpdateResource(resource);
@@ -37,6 +44,22 @@ namespace LastOutsiderServerTester.Test
             Assert(resource.Food + (resource.FoodRecoveryAmount * 2), recovery.Food);
             Assert(resource.Time + (resource.TimeRecoveryAmount * 2), recovery.Time);
             Assert(resource.Money + (resource.MoneyRecoveryAmount * 2), recovery.Money);
+            Console.WriteLine("Test 6 Minute OK");
+
+            Console.WriteLine("Test Recovery Limit");
+            resource = ServerDataBase.Instance.GetResource(account.Id);
+            resource.Food = resource.ResourceRecoverMax - 4;
+            resource.Money = resource.ResourceRecoverMax + 5; //과금 이나 원정등으로 자동회복치 초과, 이 경우 리셋되면 안됨
+            resource.NextRecoverTime = DateTime.UtcNow.AddMinutes(-3);
+            ServerDataBase.Instance.UpdateResource(resource);
+            recovery = ServerDataBase.Instance.GetResource(account.Id);
+
+            Assert(resource.Electric + (resource.ElectricRecoveryAmount * 2), recovery.Electric);
+            Assert(resource.ResourceRecoverMax, recovery.Food);
+            Assert(resource.Time + (resource.TimeRecoveryAmount * 2), recovery.Time);
+            Assert(resource.ResourceRecoverMax + 5, recovery.Money);
+
+            Console.WriteLine("Test Recovery Limit OK");
         }
     }
 }
