4f2700d404d6f40fc2fde9528f0a83606c66ce8f
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-26 00:06:14 +0900

    Add Passive

---
+++ LastOutsiderShared/Data/Battle/Passive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LastOutsiderShared.Data.Battle
+{
+    /// <summary>
+    /// 배틀중 자동으로 적용되는 스킬
+    /// </summary>
+    public abstract class Passive
+    {
+        public abstract string Name {
+            get;
+        }
+
+        public abstract string Descrption {
+            get;
+        }
+
+        public virtual void OnWaveStart()
+        {
+
+        }
+    }
+}
