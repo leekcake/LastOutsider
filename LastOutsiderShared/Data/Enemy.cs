19324224b0be4dd1228880a04c157b20b157076e
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-25 23:55:47 +0900

    Add Enemy / Stage

---
+++ LastOutsiderShared/Data/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using LastOutsiderShared.Data.Battle;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LastOutsiderShared.Data
+{
+    [MessagePackObject]
+    /// <summary>
+    /// 전투에 참가할 수 있는 적 정보
+    /// </summary>
+    public class Enemy
+    {
+        [Key(0)]
+        public int Index {
+            get; set;
+        }
+
+        [Key(1)]
+        public int Level {
+            get; set;
+        }
+
+        [Key(2)]
+        public Stats Stat {
+            get; set;
+        }
+    }
+}
---
+++ LastOutsiderShared/Data/Stage.cs
@@ -0,0 +1,22 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LastOutsiderShared.Data
+{
+    [MessagePackObject]
+    /// <summary>
+    /// 전투가 진행되는 스테이지 정보
+    /// </summary>
+    public class Stage
+    {
+        [Key(0)]
+        public int WaveCount {
+            get; set;
+        }
+
+        [Key(1)]
+        public List<Enemy[]> Enemies;
+    }
+}
