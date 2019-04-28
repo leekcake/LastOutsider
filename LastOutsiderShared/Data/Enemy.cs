0e8d2c2838f38349adef583290ab0914cae989a7
Blue Electric <fkdlxmslrx@gmail.com>
2019-04-28 23:43:34 +0900

    Add FixedData of Enemy

--- LastOutsiderShared/Data/Enemy.cs
+++ LastOutsiderShared/Data/Enemy.cs
@@ -16,6 +16,33 @@ namespace LastOutsiderShared.Data
         public static readonly string SPAGHETTI_CODE_KEY = "Enemy/Spaghetti Code/";
         public static readonly string SIDE_EFFECT_CODE_KEY = "Enemy/Side Effect Code/";
 
+        public abstract class FixedData : FightableActor.FixedData
+        {
+            #region Enum
+            public enum Type
+            {
+                Bug = 0, //Attacker
+                SpaghettiCode = 1, //Defenser
+                SideEffectCode = 2 //Supporter
+            }
+            #endregion
+
+            public Type TypeData {
+                get; protected set;
+            }
+
+            public Skill[] Skills {
+                get; protected set;
+            }
+
+            public Passive[] Passives {
+                get; protected set;
+            }
+
+            public abstract string Key { get; }
+            public abstract string Name { get; }
+        }
+
         [Key(0)]
         public int Index {
             get; set;
